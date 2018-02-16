20e4173ca44f9cb3fb57275483b43c8215a76ee3
Olivier Mengué <dolmen@cpan.org>
2018-02-16 10:49:28 +0100

    On go1.10 use the Encoder/Decoder that is now in stdlib

---
+++ decoder_go1.10.go
@@ -0,0 +1,21 @@
+//+build go1.10
+
+package hexenc
+
+import (
+	"encoding/hex"
+	"io"
+)
+
+// NewEncoder returns an io.Writer that writes lowercase hexadecimal characters
+// to w.
+func (Encoding) NewEncoder(w io.Writer) io.Writer {
+	return hex.NewEncoder(w)
+}
+
+// NewDecoder returns an io.Reader that decodes hexadecimal characters from r.
+// NewDecoder expects that r contain only an even number of hexadecimal
+// characters.
+func (Encoding) NewDecoder(r io.Reader) io.Reader {
+	return hex.NewDecoder(r)
+}
